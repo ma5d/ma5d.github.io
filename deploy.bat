ba916d962957134ea743f014586bbd667bae67c1
looktheworld <2689169757@qq.com>
2024-03-28 21:12:47 +0800

    test

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+start npm run build
+timeout /t 40 /nobreak
+cd build
+git init
+git add .
+git commit -m "build"
+git branch -M build
+git remote add origin https://github.com/horse-world/horse-world.github.io.git
+git push -f -u origin build
+cd ..
+echo https://horse-world.github.io/kaiteui/index.html
